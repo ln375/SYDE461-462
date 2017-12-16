a34c49fbd3ad97ab04cd7421d29c207f7fe81032
chachao <charischao@gmail.com>
2017-12-15 19:34:12 -0500

    Adding functionality to allow for database upgrades

--- TransporterApp/app/src/main/java/com/transporterapp/syde/transporterapp/DatabaseOpenHelper.java
+++ TransporterApp/app/src/main/java/com/transporterapp/syde/transporterapp/DatabaseOpenHelper.java
@@ -2,6 +2,15 @@ package com.transporterapp.syde.transporterapp;
 
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 /**
@@ -10,9 +19,69 @@ import android.content.Context;
 
 public class DatabaseOpenHelper extends SQLiteAssetHelper {
     private static final String DATABASE_NAME = "demoData.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
+    private static final String TAG = DatabaseOpenHelper.class.getName();
+    private final Context context;
 
     public DatabaseOpenHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.e(TAG, "Updating table from " + oldVersion + " to " + newVersion);
+
+        try {
+            for (int i = oldVersion; i < newVersion; ++i) {
+                String migrationName = String.format("from_%d_to_%d.sql", i, (i + 1));
+                Log.d(TAG, "Looking for migration file: " + migrationName);
+                readAndExecuteSQLScript(db, context, migrationName);
+            }
+        } catch (Exception exception) {
+            Log.e(TAG, "Exception running upgrade script:", exception);
+        }
+    }
+
+    private void readAndExecuteSQLScript(SQLiteDatabase db, Context ctx, String fileName) throws IOException {
+        if (TextUtils.isEmpty(fileName)) {
+            Log.d(TAG, "SQL script file name is empty");
+            return;
+        }
+
+        Log.d(TAG, "Script found. Executing...");
+        AssetManager assetManager = ctx.getAssets();
+        String imgPath[] = assetManager.list("upgradeScripts");
+        BufferedReader reader = null;
+
+        try {
+            InputStream is = assetManager.open("upgradeScripts/" + fileName);
+            InputStreamReader isr = new InputStreamReader(is);
+            reader = new BufferedReader(isr);
+            executeSQLScript(db, reader);
+        } catch (IOException e) {
+            Log.e(TAG, "IOException:", e);
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    Log.e(TAG, "IOException:", e);
+                }
+            }
+        }
+    }
+
+    private void executeSQLScript(SQLiteDatabase db, BufferedReader reader) throws IOException {
+        String line;
+        StringBuilder statement = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            statement.append(line);
+            statement.append("\n");
+            if (line.endsWith(";")) {
+                db.execSQL(statement.toString());
+                statement = new StringBuilder();
+            }
+        }
     }
 }
