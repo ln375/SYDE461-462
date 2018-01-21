c5b014597a8edf12b27ba17a6b69c8c337cd6e16
chachao <charischao@gmail.com>
2018-01-21 16:57:04 -0500

    Removing unnecessary files

--- TransporterApp/app/src/main/java/com/transporterapp/syde/transporterapp/CollectMilk/MilkEntryFrag.java
+++ TransporterApp/app/src/main/java/com/transporterapp/syde/transporterapp/CollectMilk/MilkEntryFrag.java
@@ -1,8 +1,10 @@
 package com.transporterapp.syde.transporterapp.CollectMilk;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,18 +14,23 @@ import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.transporterapp.syde.transporterapp.R;
 import com.transporterapp.syde.transporterapp.databases.DataBaseUtil;
+import com.transporterapp.syde.transporterapp.databases.DatabaseConstants;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MilkEntryFrag extends Fragment {
 
-    EditText milkVolume;
-    Button SaveData;
-    RadioGroup smellTest;
-    RadioGroup densityTest;
+    private static List<String> milkEntryColumns = Arrays.asList("MILK_WEIGHT", "SMELL", "DENSITY");
+
+    private EditText milkVolume;
+    private Button SaveData;
+    private RadioGroup smellTest;
+    private RadioGroup densityTest;
 
     public MilkEntryFrag() {
     }
@@ -49,8 +56,87 @@ public class MilkEntryFrag extends Fragment {
 
         jugDropdown.setAdapter(jugListAdapter);
 
+        //Edit data fields
+        smellTest = (RadioGroup) view.findViewById(R.id.smell_test);
+        densityTest = (RadioGroup) view.findViewById(R.id.density_test);
+        milkVolume = (EditText) view.findViewById(R.id.milk_volume);
+        SaveData = (Button)view.findViewById(R.id.save_data);
+
+        AddData();
+
         return view;
+    }
 
+    public  void AddData() {
+        SaveData.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int smellIndex = smellTest.indexOfChild(getView().findViewById(smellTest.getCheckedRadioButtonId()));
+                        int densityIndex = densityTest.indexOfChild(getView().findViewById(densityTest.getCheckedRadioButtonId()));
+                        boolean isInserted = insertData(milkVolume.getText().toString(), smellIndex, densityIndex);
+                        if(isInserted == true) {
+                            Toast.makeText(getContext(),"Data Inserted", Toast.LENGTH_LONG).show();
+                            Cursor res = DataBaseUtil.selectStatement(DatabaseConstants.tbltrFarmerTransporter, "","",  "", getContext());
+                            if(res.getCount() == 0) {
+                                // show message
+                                showMessage("Error","Nothing found");
+                                return;
+                            }
 
+                            StringBuffer buffer = new StringBuffer();
+                            while (res.moveToNext()) {
+                                buffer.append("Id :"+ res.getString(0)+"\n");
+                                buffer.append("Milk Weight :"+ res.getString(6)+"\n");
+                                buffer.append("Density :"+ getQualityRating(res.getString(10))+"\n");
+                                buffer.append("Smell :"+ getQualityRating(res.getString(8))+"\n\n");
+                            }
+
+                            // Show all data
+                            showMessage("Data",buffer.toString());
+
+                        } else {
+                            Toast.makeText(getContext(),"Data not Inserted",Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                }
+        );
+
+    }
+
+    public void showMessage(String title,String Message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
+    }
+
+    public String getQualityRating(String index) {
+        String temp = "";
+        switch (index) {
+            case "0":
+                temp = "Good";
+                break;
+            case "1":
+                temp = "Okay";
+                break;
+            case "2":
+                temp = "Bad";
+                break;
+            case "3":
+                temp = "N/A";
+                break;
+            default:
+                temp = "N/A";
+
+        }
+        return temp;
+    }
+
+    public boolean insertData(String milkData, int smellIndex, int densityIndex) {
+        List<String> values = Arrays.asList(milkData, String.valueOf(smellIndex), String.valueOf(densityIndex));
+        return DataBaseUtil.insertStatement(DatabaseConstants.tbltrFarmerTransporter, milkEntryColumns, values, getContext());
     }
 }
