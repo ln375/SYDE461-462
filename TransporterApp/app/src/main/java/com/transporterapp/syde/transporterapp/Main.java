aad59ab66b7d411a1d25db2a6c7c3d2edeb98603
chachao <charischao@gmail.com>
2018-03-18 18:39:14 -0400

    Making disabling fields more robust

--- TransporterApp/app/src/main/java/com/transporterapp/syde/transporterapp/Main.java
+++ TransporterApp/app/src/main/java/com/transporterapp/syde/transporterapp/Main.java
@@ -209,7 +209,7 @@ public class Main extends AppCompatActivity
         Fragment frag = fragmentManager.findFragmentById(R.id.container);
         if(frag instanceof MilkEntryFrag){
             if (((MilkEntryFrag) frag).areDataFieldsEmpty()) {
-                ((MilkEntryFrag) frag).clearData();
+                //((MilkEntryFrag) frag).clearData();
                 super.onBackPressed();
             } else {
                 showUnsavedDataMessage(((MilkEntryFrag) frag), this);
@@ -352,7 +352,7 @@ public class Main extends AppCompatActivity
             switch (which){
                 case DialogInterface.BUTTON_POSITIVE:
                     Fragment frag = fragmentManager.findFragmentById(R.id.container);
-                    ((MilkEntryFrag) frag).clearData();
+                    //((MilkEntryFrag) frag).clearData();
                     Main.super.onBackPressed();
                     break;
 
